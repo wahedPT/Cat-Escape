8c4b6460771f004069070ce70958fef31af4f3be
wahedxcube <wahedalikhan.md@xcubelabs.com>
2021-08-02 04:32:56 +0530

    AI Navigation done

---
+++ Assets/EnenyM.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnenyM : MonoBehaviour
+{
+    public GameObject player;
+    public float Distance;
+
+    public bool Istaret;
+    public NavMeshAgent navMesh;
+     void Start()
+    {
+       
+    }
+
+    void Update()
+    {
+        Distance = Vector3.Distance(player.transform.position, this.transform.position);
+
+        if(Distance<=5)
+        {
+            Istaret = false;
+        }
+        if (Distance <= 5)
+        {
+            Istaret = true;
+        }
+        if(Istaret)
+        {
+            navMesh.isStopped = false;
+            navMesh.SetDestination(player.transform.position);
+        }
+        if (!Istaret)
+        {
+            navMesh.isStopped = true;
+        }
+    }
+   
+}
